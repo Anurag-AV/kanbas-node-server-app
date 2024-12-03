de62e72c46f155e06ed039d82006a66ace06d810
Anurag AV <anuragkrishnanav@gmail.com>
2024-12-02 20:30:45 -0500

    db backend

--- Kanbas/Enrollments/route.js
+++ Kanbas/Enrollments/route.js
@@ -7,7 +7,7 @@ export default function EnrollmentRoutes(app) {
  });
  app.post("/api/enrollments/enroll", async (req, res) => {
     const {userId, courseId} = req.body;
-    const status = await EnrollmentRoutes.enrollUserInCourse(userId, courseId);
+    const status = await EnrollmentsDao.enrollUserInCourse(userId, courseId);
     res.send(status);
   });
 }
